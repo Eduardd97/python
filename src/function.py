4694fb5324b8a649a3f376546b21d12fb56b61a5
PyThon <kozeychukk1997@gmail.com>
2021-10-21 13:57:46 +0300

    string method "format" 2

---
+++ src/function.py
@@ -0,0 +1,5 @@
+def my_function(fname):
+    print(fname + " Refsnes")
+
+
+my_function("Eduard")
--- src/temp.py
+++ src/temp.py
@@ -1,7 +1,7 @@
 a = input("Введите ваше имя: ")
 b = input("Введите ваше фамилию: ")
-c = "Hi i'm {} {}, nice to meet you"
-print(c.format(a, b))
+c = "Hi i'm {1} {0}, nice to meet you"
+print(c.format(b, a))
 txt0 = input("Введите ваше имя: ")
 txt1 = input("Введите ваше фамилию: ")
 txt2 = "Me too, i'm {} {} "
