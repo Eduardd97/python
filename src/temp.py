7f9c80cc3e97dbecca4fde2810b9ae6c3a05bfe5
PyThon <kozeychukk1997@gmail.com>
2021-10-21 13:45:52 +0300

    string method "format"

---
+++ src/temp.py
@@ -0,0 +1,9 @@
+a = input("Введите ваше имя: ")
+b = input("Введите ваше фамилию: ")
+c = "Hi i'm {} {}, nice to meet you"
+print(c.format(a, b))
+txt0 = input("Введите ваше имя: ")
+txt1 = input("Введите ваше фамилию: ")
+txt2 = "Me too, i'm {} {} "
+print(txt2.format(txt0, txt1))
+
