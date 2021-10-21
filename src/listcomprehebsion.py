9d775d191d4c0a4789056058c2670483ea04c3eb
PyThon <kozeychukk1997@gmail.com>
2021-10-21 15:36:59 +0300

    listcomprehenion

---
+++ src/listcomprehebsion.py
@@ -0,0 +1,6 @@
+TOYS = ["Часы price", "Телефон Samsung", "Ремешок price", "Украшение price", "Ручка Parker",
+       "Футболка Lakoste price", "Джынсы Diesel", "Чехол Для iPhone", "Ноутбук price", "Кроссовки price"]
+TOY = ["price", "Diesel"]
+print([li.upper() for li in TOYS if "price" in li])
+print([li1.lower() for li1 in TOYS if "price" not in li1])
+
